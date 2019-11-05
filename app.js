b86f8d184a275adf218146cd4b66d9c349b66f8e
mohamed.labouardy@gmail.com <mohamed@labouardy.com>
2019-11-05 15:31:53 +0100

    set url

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ angular.module('demo', [])
     .controller('MainCtrl', function ($scope, $http) {
         $scope.movie = {};
         $scope.name = '';
-        $http.get('/search/Gladiator').then(function (resp) {
+        $http.get('https://g9iufmmam1.execute-api.eu-central-1.amazonaws.com/sandbox/search/Gladiator').then(function (resp) {
             $scope.movie = resp.data;
         });
-        $http.get('/env').then(function (resp) {
+        $http.get('https://g9iufmmam1.execute-api.eu-central-1.amazonaws.com/sandbox/env').then(function (resp) {
             $scope.environment = resp.data.environment;
         });
         $scope.search = function () {
-            $http.get('/search/' + $scope.name).then(function (resp) {
+            $http.get('https://g9iufmmam1.execute-api.eu-central-1.amazonaws.com/sandbox/search/' + $scope.name).then(function (resp) {
                 $scope.movie = resp.data;
             });
         }
     })
+
+
+
