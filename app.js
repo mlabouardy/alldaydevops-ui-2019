2658039e16d3182d1d3bb490a2b3f3b369ef2fd4
LABOUARDY Mohamed <mohamed@labouardy.com>
2019-11-05 15:36:50 +0100

    fix url

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ angular.module('demo', [])
     .controller('MainCtrl', function ($scope, $http) {
         $scope.movie = {};
         $scope.name = '';
-        $http.get('/search/Gladiator').then(function (resp) {
+        $http.get('https://g9iufmmam1.execute-api.eu-central-1.amazonaws.com/staging/search/Gladiator').then(function (resp) {
             $scope.movie = resp.data;
         });
-        $http.get('/env').then(function (resp) {
+        $http.get('https://g9iufmmam1.execute-api.eu-central-1.amazonaws.com/staging/env').then(function (resp) {
             $scope.environment = resp.data.environment;
         });
         $scope.search = function () {
-            $http.get('/search/' + $scope.name).then(function (resp) {
+            $http.get('https://g9iufmmam1.execute-api.eu-central-1.amazonaws.com/staging/search/' + $scope.name).then(function (resp) {
                 $scope.movie = resp.data;
             });
         }
